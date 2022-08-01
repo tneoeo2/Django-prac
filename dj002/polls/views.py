1fa6057d679af0eccedce097ed47d677e1c2697b
tneoeo2 <tneoeo2@gmail.com>
2022-08-01 16:31:22 +0900

    render 수정

--- dj002/polls/views.py
+++ dj002/polls/views.py
@@ -6,14 +6,14 @@ from .models import Question
 
 def index(request):
   latest_question_list = Question.objects.order_by('-pub_date')[:5]     #최신순으로 정렬 5개의 -> 최신 5개 질문 뽑기
-  template = loader.get_template('polls/index.html')
+  # template = loader.get_template('polls/index.html')
   context = {
     'latest_question_list': latest_question_list,
   }
   
-  return HttpResponse(template.render(context, request))
+  return render(request, 'polls/index.html', context)    #request, 템플릿 이름 , context(사전형 객체: 선택적 인수)
+  # return HttpResponse(template.render(context, request)) #
   # return HttpResponse("Hello, world! Your're at the polls index.")
-
 
 def detail(request, question_id):
   return HttpResponse("You're looking at question %s." % question_id)
