ce4fcb075a718d10ef30ddcf68d71e036a610e0d
tneoeo2 <tneoeo2@gmail.com>
2022-08-01 16:49:03 +0900

    404에러 추가

--- dj002/polls/views.py
+++ dj002/polls/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
-from django.template import loader
+# from django.template import loader
+from django.http import Http404
 
 from .models import Question
 
@@ -16,7 +17,9 @@ def index(request):
   # return HttpResponse("Hello, world! Your're at the polls index.")
 
 def detail(request, question_id):
-  return HttpResponse("You're looking at question %s." % question_id)
+  question = get_object_or_404(Question, pk=question_id)
+
+  return render(request, 'polls/detail.html', {'question':question})
 
 def results(request, question_id):  
   response = "You're looking at the results of question %s."
@@ -24,4 +27,4 @@ def results(request, question_id):
 
 def vote(request, question_id):
   return HttpResponse("You're voting on question %s." % question_id)
-                      
+                  
