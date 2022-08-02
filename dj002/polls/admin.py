1e57f174f91dde5bd913193e403d9677430a634c
tneoeo2 <tneoeo2@gmail.com>
2022-08-02 15:08:47 +0900

    admin Choice Question에서 관리하게 수정

--- dj002/polls/admin.py
+++ dj002/polls/admin.py
@@ -1,17 +1,25 @@
 from django.contrib import admin
 from .models import Question, Choice      #admin에서 제어할 권한을 부여
 
-
-
+class ChoiceInline(admin.TabularInline):
+  model = Choice
+  extra = 3         #기본으로 3가지 선택 항목 제공
 
 class QuestionAdmin(admin.ModelAdmin):
   #여러개의 필드가 있는 폼에서는 fieldset으로 분할하여 관리
   #fieldset [(fieldset 제목, )]
-  fieldsets = [(None, {'fields':['question_text'], }),
-               ('Date information', {'fields': ['pub_date']}), ]
+  fieldsets = [(None, {'fields':['question_text'], }),  
+               ('Date information', {'fields': ['pub_date'], 
+                'classes' : ['collapse']}),     #
+               ]
+  
+  inlines = [ChoiceInline]    #Choice 객체는 Question 관리자 페이지에서 편집됨
+    
+  list_display = ('question_text', 'pub_date', 'was_published_recently')   #list보기에서 표시할 항목 설정
+  
+  
 
 admin.site.register(Question, QuestionAdmin)    #QuestionAdmin 두번째 인수로 전달 -> 발행일 설문 필드 앞으로 오게함
-admin.site.register(Choice)
 
 
 
