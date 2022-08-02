3849d6712596a59c4614aba6d7421bbf537afd97
tneoeo2 <tneoeo2@gmail.com>
2022-08-02 10:17:22 +0900

    generic view

--- dj002/polls/views.py
+++ dj002/polls/views.py
@@ -1,33 +1,32 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 # from django.template import loader
 # from django.http import Http404
 
 from .models import Choice, Question
 
-def index(request):
-  latest_question_list = Question.objects.order_by('-pub_date')[:5]     #최신순으로 정렬 5개의 -> 최신 5개 질문 뽑기
-  # template = loader.get_template('polls/index.html')
-  context = {
-    'latest_question_list': latest_question_list,
-  }
+##Generic으로 변경
+##ListView : 객체 목록 표시     -> <app name>/<model name>_list.html 템플릿 사용 
+##DetailView : 특정 개체 유형 세부 정보 페이지 표시   -> <app name>/<model name>_detail.html 템플릿을 사용
+class IndexView(generic.ListView):    
+  template_name = 'polls/index.html'        #기본이름 양식(_list)대신 다른 이름 사용할 때 사용
+  context_object_name = 'latest_question_list'    #자동으로 생성되는 컨텍스트 변수 덮어쓰기(question_list 덮어쓰기)
+
+  def get_queryset(self):
+    return Question.objects.order_by('-pub_date')[:5]     #최신순으로 정렬 5개의 -> 최신 5개 질문 뽑기
   
-  return render(request, 'polls/index.html', context)    #request, 템플릿 이름 , context(사전형 객체: 선택적 인수)
-  # return HttpResponse(template.render(context, request)) #
-  # return HttpResponse("Hello, world! Your're at the polls index.")
-
-def detail(request, question_id):
-  question = get_object_or_404(Question, pk=question_id)
-
-  return render(request, 'polls/detail.html', {'question':question})
-
-def results(request, question_id):  
-  question = get_object_or_404(Question, pk=question_id)
-  return render(request, 'polls/results.html', {'question':question})
-
+class DetailView(generic.DetailView):
+  model = Question
+  template_name = "polls/detail.html" 
+  
+class ResultsView(generic.DetailView):
+  model = Question
+  template_name = 'polls/results.html'
+  
 def vote(request, question_id):
-  question = get_object_or_404(Question, pk=question_id)  
+  question = get_object_or_404(Question, pk=question_id)      #pk가 없을 시 404페이지를 반환한다.
   try:
     #request.POST는 키로 전송된 자료에 접근할 수 있도록 해주는 사전과 같은 객체
     selected_choice = question.choice_set.get(pk=request.POST['choice'])   #request.POST['choice']: 선택된 설문의 ID를 문자열로 반환
