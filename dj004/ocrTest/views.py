53c3cfd41abdef3669370f75e9c1a0ee9fd7a859
tneoeo2 <tneoeo2@gmail.com>
2022-08-03 18:06:39 +0900

    test

--- dj004/ocrTest/views.py
+++ dj004/ocrTest/views.py
@@ -1,12 +1,21 @@
 from django.utils import timezone
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from requests import post
 
 from .models import Post
 
-
+##후에 api로 ocr 결과 받아주는걸로 변경(사진 저장X)
 def index(request):
-  
-  return render(request, 'ocrTest/index.html')
+  if request.method =="POST":
+    print("POST request")
+    if request.FILES['mainphoto']:
+      print("FILES request")
+      new_photo = Post.objects.create(
+        # postname=request.POST['photoname'],
+        mainphoto=request.FILES['mainphoto']
+      )
+      print("image: ", request.FILES['mainphoto'])
+  return render(request, 'ocrTest/index.html', {'mainphoto': request.FILES['mainphoto']})
