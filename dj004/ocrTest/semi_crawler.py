b4f4c48d9edb8104dacb2e86aa379fc02dc517f8
tneoeo2 <tneoeo2@gmail.com>
2022-08-22 00:04:15 +0900

    예매기능 추가 + 비밀번호 안보이게 수정

--- dj004/ocrTest/semi_crawler.py
+++ dj004/ocrTest/semi_crawler.py
@@ -1,16 +1,22 @@
 import os
 import sys
 import time
-import warnings
+import getpass
+from tkinter.tix import Tree
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys 
 from selenium.webdriver.support.select import Select
 from bs4 import BeautifulSoup as bs
 
+print("id 입력 : ", end='')
+user_id = input()
+user_pw = getpass.getpass()
+
+
 USER_INFO = {
-    "id": "userid"
-    ,"pw": "userpw"
+    "id": user_id
+    ,"pw": user_pw
     
 }
 
@@ -22,33 +28,43 @@ def run():
     chrome_option.add_experimental_option("detach", True)
     # chrome_option.add_argument("headless")   #*창안보이게 실행
     # chrome_option.add_experimental_option('excludeSwitches', ['enable-logging'])
-    driver = webdriver.Chrome('S://CODE//Django//crawler//chromedriver.exe', chrome_options=chrome_option)
+    driver = webdriver.Chrome('crawler/chromedriver.exe', chrome_options=chrome_option)
     # driver = webdriver.PhantomJS(ptjs)
     
     return driver
 
-def run_browser(driver):
-    url = "https://www.yes24.com/Templates/FTLogin.aspx"
-    # url = "https://ezsso.bizmeka.com/loginForm.do"
 
-    driver.implicitly_wait(3) 
-
-    driver.get(url)
-    driver.find_element(By.ID,"SMemberID").send_keys(USER_INFO['id'])
-    driver.find_element(By.ID,"SMemberPassword").send_keys(USER_INFO['pw'])
-    driver.find_element(By.ID,"btnLogin").click()
-
-    #팝업창 대응
-    popups = driver.find_elements(By.CLASS_NAME, "yPopBot")
-    # print(popups, len(popups))
-    if len(popups) != 0:
-        for pop in popups:
-            try:
-                pop.find_element(By.ID, "chk_info").click()    #팝업창 닫기 버튼이 있을 시 클릭이벤트
-                print("popup element: ", pop)
-            except:
-                pass
+def get_seat(driver):    #자리줍줍
+    t_month = "9"   #월
+    t_date = "4"    #일
+    num = "1"    #회차 1, 2
+    
+    driver.get("http://ticket.yes24.com/Perf/42489?Gcode=009_400")    #원하는 공연번호
+    #월 맞는지 확인
+    month = driver.find_element(By.CLASS_NAME, "ui-datepicker-month").text
+    
+    #팝업창 있다면
+    try:
+        popup = driver.find_element(By.CLASS_NAME,"pop-alert-check")
+        popup.find_element(By.TAG_NAME, 'a').click()
+    except:
+        pass
+    
+    while True:
+        if int(month) == int(t_month):
+            calendar = driver.find_element(By.CLASS_NAME, "ui-datepicker-calendar")
+            calendar.find_element(By.PARTIAL_LINK_TEXT, t_date).click()
+            numlist = driver.find_element(By.ID, "PerfPlayTime")
+            numlist.find_element(By.PARTIAL_LINK_TEXT, num+"회").click()
+            driver.find_element(By.PARTIAL_LINK_TEXT, "예매하기").click()   #예매하기
+            break
+        else:
+            time.sleep(2)
+            driver.find_element(By.CLASS_NAME, "ui-icon-circle-triangle-e").click()
+            month = driver.find_element(By.CLASS_NAME, "ui-datepicker-month").text
             
+def get_rank(driver):
+                
     #티켓창으로 이동
     driver.get("http://ticket.yes24.com/")
     
@@ -82,8 +98,31 @@ def run_browser(driver):
     print("rank_list: ",rank_list)
     
     
-    return rank_best, rank_list
-    # time.sleep(500)
+
+def run_browser(driver):
+    url = "https://www.yes24.com/Templates/FTLogin.aspx"
+    # url = "https://ezsso.bizmeka.com/loginForm.do"
+
+    driver.implicitly_wait(3) 
+
+    driver.get(url)
+    driver.find_element(By.ID,"SMemberID").send_keys(USER_INFO['id'])
+    driver.find_element(By.ID,"SMemberPassword").send_keys(USER_INFO['pw'])
+    driver.find_element(By.ID,"btnLogin").click()
+
+    #팝업창 대응
+    popups = driver.find_elements(By.CLASS_NAME, "yPopBot")
+    # print(popups, len(popups))
+    if len(popups) != 0:
+        for pop in popups:
+            try:
+                pop.find_element(By.ID, "chk_info").click()    #팝업창 닫기 버튼이 있을 시 클릭이벤트
+                print("popup element: ", pop)
+            except:
+                pass
+    get_seat(driver) 
+    # get_rank(driver)            
+    time.sleep(500)
     
     driver.quit()
     while True:     #브라우저 자동종료 방지 코드
