1ca1f3ca0fee22f979a1e67967ba95945935d18f
tneoeo2 <tneoeo2@gmail.com>
2022-08-22 14:07:06 +0900

    ranking DB 추가

--- dj004/ocrTest/models.py
+++ dj004/ocrTest/models.py
@@ -1,3 +1,4 @@
+from email.mime import image
 from django.db import models
 
 # Create your models here.
@@ -7,3 +8,24 @@ class Post(models.Model):
 
   def __str__(self):
     return self.photoname
+
+class RankPost(models.Model):
+  pub_date = models.DateTimeField("data published...")
+  
+  def __str__(self):
+    return self.pub_date
+  
+
+class Ranking(models.Model):
+  pub_date = models.ForeignKey(RankPost, on_delete=models.CASCADE)
+  title = models.CharField(max_length=100)
+  image = models.ImageField(blank=True, null=True, upload_to='rank')
+  start_date = models.DateField()         #시작 날짜
+  end_date = models.DateField()           #끝 날짜
+  place = models.CharField(max_length=30)
+  ranking = models.CharField(max_length=10)
+  
+  def __str__(self):
+    return self.title
+  
+  
