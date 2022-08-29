24fbda060e62890cc3d16894147fd75c493408a4
tneoeo2 <tneoeo2@gmail.com>
2022-08-29 16:49:41 +0900

    views 수정 재사용가능한 template으로 분리

--- dj005/lifeapp/views.py
+++ dj005/lifeapp/views.py
@@ -1,13 +1,59 @@
-from django.shortcuts import render, HttpResponse
 import random
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+
+topics = [
+  {'id': 1, 'title': 'routing', 'body': 'Routing is ..'},
+  {'id': 2, 'title': 'view', 'body': 'View is ..'},
+  {'id': 3, 'title': 'Model', 'body': 'Model is ..'},
+]
+
+def HTMLTemplate(articleTag):     #재사용가능한 템플릿으로 분리
+  global topics
+  ol = ''
+  for topic in topics:
+    ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'        
+  return f'''
+            <html>
+            <body>
+              <h1><a href ="/">Django</a></h1>                      
+              <ul>
+                {ol}
+              </ul>
+              {articleTag}
+              <ul>
+                <li><a href="/create/">create</a></li>
+              </ul>
+            </body>
+            </html>
+            '''
+
 def index(request):     #request : 요청과 관련된 객체(이름 바꿔도 가능하나 관습적으로 request 사용)
-  return HttpResponse("Welcome!!")
+  article = '''
+    <h2>Welcome</h2>
+    Hello, Django
+    <p>This is article Tag.</p>
+  '''
+  return HttpResponse(HTMLTemplate(article))
 
+  
 def create(request):
-  return HttpResponse("Create")
+  article = '''
+  <form action="/create">
+    <p><input type="text" name="title" placeholder="title..."></p>
+    <p><textarea name="body" placeholder="body..."></textarea></p>
+    <P><input type="submit"></p>
+  </form>
+  '''
+  
+  return HttpResponse(HTMLTemplate(article))
   
 def read(request, id):
-  return HttpResponse("Read!" + id)
+  global topics 
+  article = ''
+  for topic in topics: 
+    if topic['id'] == int(id):    #선택한 id에 해당하는 페이지에 body 값 가져와 띄우기
+      article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
+  
+  return HttpResponse(HTMLTemplate(article))
 
